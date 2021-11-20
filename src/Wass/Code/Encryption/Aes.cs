74f866c5a0d5f52adfada340982e076121504366
Matthew Dove <matthew.dove@globalpayaunz.cloud>
2021-11-20 20:16:26 +1000

    Added the salt into the ciphertext.

--- src/Wass/Code/Encryption/Aes.cs
+++ src/Wass/Code/Encryption/Aes.cs
@@ -1,13 +1,12 @@
 ﻿using System.Security.Cryptography;
-using System.Text;
 using Sea = System.Security.Cryptography.Aes;
 
 namespace Wass.Code.Encryption
 {
     public sealed class Aes
     {
-        public byte[] Encrypt(string key, byte[] plainbytes) => EncryptPlaintext(key, plainbytes);
-        public byte[] Decrypt(string key, byte[] cipherbytes) => DecryptCiphertext(key, cipherbytes);
+        public byte[] Encrypt(string password, byte[] plainbytes) => EncryptPlaintext(password, plainbytes);
+        public byte[] Decrypt(string password, byte[] cipherbytes) => DecryptCiphertext(password, cipherbytes);
 
         private const int _iterations = 10000;
         private const int _saltSize = 16;
@@ -15,12 +14,11 @@ namespace Wass.Code.Encryption
         private const int _ivSize = 16;
         private static readonly HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA512;
 
-        private static readonly byte[] _salt = RandomNumberGenerator.GetBytes(_saltSize);
-
-        private static byte[] EncryptPlaintext(string key, byte[] plainbytes)
+        private static byte[] EncryptPlaintext(string password, byte[] plainbytes)
         {
             using var sea = Sea.Create();
-            using var crypto = new Rfc2898DeriveBytes(key, _salt, _iterations, hashAlgorithm);
+            var salt = RandomNumberGenerator.GetBytes(_saltSize);
+            using var crypto = new Rfc2898DeriveBytes(password, salt, _iterations, hashAlgorithm);
             sea.Key = crypto.GetBytes(_keySize);
             sea.IV = crypto.GetBytes(_ivSize);
             using var ms = new MemoryStream();
@@ -28,18 +26,25 @@ namespace Wass.Code.Encryption
             cs.Write(plainbytes, 0, plainbytes.Length);
             cs.Close();
             var cipherbytes = ms.ToArray();
-            return cipherbytes;
+            var buffer = new byte[cipherbytes.Length + _saltSize];
+            Array.Copy(salt, buffer, _saltSize);
+            Array.Copy(cipherbytes, 0, buffer, _saltSize, cipherbytes.Length);
+            return buffer;
         }
 
-        private static byte[] DecryptCiphertext(string key, byte[] cipherbytes)
+        private static byte[] DecryptCiphertext(string password, byte[] cipherbytes)
         {
             using var sea = Sea.Create();
-            using var crypto = new Rfc2898DeriveBytes(key, _salt, _iterations, hashAlgorithm);
+            var salt = new byte[_saltSize];
+            var cipher = new byte[cipherbytes.Length - _saltSize];
+            Array.Copy(cipherbytes, 0, salt, 0, _saltSize);
+            Array.Copy(cipherbytes, _saltSize, cipher, 0, cipher.Length);
+            using var crypto = new Rfc2898DeriveBytes(password, salt, _iterations, hashAlgorithm);
             sea.Key = crypto.GetBytes(_keySize);
             sea.IV = crypto.GetBytes(_ivSize);
             using var ms = new MemoryStream();
             using var cs = new CryptoStream(ms, sea.CreateDecryptor(), CryptoStreamMode.Write);
-            cs.Write(cipherbytes, 0, cipherbytes.Length);
+            cs.Write(cipher, 0, cipher.Length);
             cs.Close();
             var plainbytes = ms.ToArray();
             return plainbytes;
