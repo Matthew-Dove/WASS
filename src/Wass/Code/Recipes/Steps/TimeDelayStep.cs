7a522dce8d6d5e3165b58824901e6ef5652cd326
Matthew Dove <matthew.dove@globalpayaunz.cloud>
2021-11-27 16:15:56 +1000

    Removed sync time delay.

--- src/Wass/Code/Recipes/Steps/TimeDelayStep.cs
+++ src/Wass/Code/Recipes/Steps/TimeDelayStep.cs
@@ -4,13 +4,13 @@ namespace Wass.Code.Recipes.Steps
 {
     public sealed class TimeDelayStep : Step
     {
-        internal TimeDelayStep() : base(isAsync: false) { }
-        internal override bool Method(FileModel file, IngredientModel ingredients) => TimeDelay(file, ingredients);
-        internal override Task<bool> MethodAsync(FileModel file, IngredientModel ingredients) => throw new NotImplementedException();
+        internal TimeDelayStep() : base(isAsync: true) { }
+        internal override bool Method(FileModel file, IngredientModel ingredients) => throw new NotImplementedException();
+        internal override Task<bool> MethodAsync(FileModel file, IngredientModel ingredients) => TimeDelay(file, ingredients);
 
         private static readonly string[] _requiredIngredients = { "ms" };
 
-        private static bool TimeDelay(FileModel file, IngredientModel ingredients)
+        private static async Task<bool> TimeDelay(FileModel file, IngredientModel ingredients)
         {
             if (!file.IsValid() || !ingredients.IsValid(_requiredIngredients)) return false.Trail($"{nameof(TimeDelayStep)} validation failed.");
             var isValid = false;
@@ -20,7 +20,7 @@ namespace Wass.Code.Recipes.Steps
             {
                 if (int.TryParse(ms, out int milliseconds) && milliseconds > 0)
                 {
-                    Thread.Sleep(milliseconds);
+                    await Task.Delay(milliseconds);
                     isValid = true;
                 }
             }
